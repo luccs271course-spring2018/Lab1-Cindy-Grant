691770a6c1049d7e1ef03be4231450abd620575a
rogerld <ywang83@luc.edu>
2017-09-11 21:03:16 -0500

    Test update
    Signed-off-by: rogerld <ywang83@luc.edu>

---
+++ src/test/java/hw/FBTest.java
@@ -0,0 +1,39 @@
+package hw;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FBTest {
+
+    @Test
+    public void FB10minus() {
+        assertEquals("buzz", FB.getNumber(-10));
+    }
+
+    @Test
+    public void FB1minus() {
+        assertEquals("-1", FB.getNumber(-1));
+    }
+
+    @Test
+    public void FB0() {
+        assertEquals("0", FB.getNumber(0));
+    }
+
+    @Test
+    public void FB1() {
+        assertEquals("1", FB.getNumber(1));
+    }
+
+    @Test
+    public void FB7() {
+        assertEquals("7", FB.getNumber(7));
+    }
+
+    @Test
+    public void FB17() {
+        assertEquals("17", FB.getNumber(17));
+    }
+
+}
